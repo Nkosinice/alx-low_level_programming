1e44489ab8d1f1437d6dbd86f382dde42e18be2e
Nkosinathi <nkosinathimatutu@gmail.com>
2023-07-19 16:26:14 -0700

    alhpabet soup

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printing lowercase alphabet
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
+
