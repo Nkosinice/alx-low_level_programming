06ea276b9476ccea887311fca03a58da1679a17f
Nkosinathi <nkosinathimatutu@gmail.com>
2023-07-21 03:53:24 -0700

    finding the digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: int to be located
+ * Return: 1 if c is a digit, else Return 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
